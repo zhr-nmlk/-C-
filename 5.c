7b21d0197de8cd9d37280100d6598478b129f669
zhr_nmlk <111226611+zhr-nmlk@users.noreply.github.com>
2024-07-13 16:31:13 +0800

    Delete 5.c

--- 5.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int x;
-    printf("Enter the number of x:");
-    scanf("%d",&x);//注意不要漏掉“ & ”
-    printf("计算得到多项式的值为：%d",3*x*x*x*x*x+2*x*x*x*x-5*x*x*x-x*x+7*x-6);
-    return 0;
-}
